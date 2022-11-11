35257f37aa041ea7806f67a728dfbe76b4afdf0f
DNomadVN <vovandoan4815@gmail.com>
2022-11-11 19:31:17 +0700

    Update playerctl scipts, add click events

--- scripts/playerctl_script.sh
+++ scripts/playerctl_script.sh
@@ -1,8 +1,16 @@
 playerctl metadata title &> player.log
+playerctl status &>> player.log
 title=$(head -n 1 player.log)
+status=$(head -n 2 player.log | tail -1)
 length=${#title}
 if [[ $title != "No players found" ]]
-then 
+then
+    if [[ $status == "Playing" ]]
+    then
+        prefix=""
+    else
+        prefix=""
+    fi
     if [[ length -gt 30 ]]
     then
         echo "${title:0:30}..."
