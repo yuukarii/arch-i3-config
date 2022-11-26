6c06c4c7ff1e5f4373a45cc36d216b6e3efb2936
DNomadVN <vovandoan4815@gmail.com>
2022-11-26 08:43:01 +0700

    i3 config: work with multi-monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,4 +4,7 @@
 killall -q polybar
 
 # Launch Polybar
-polybar -c ~/.config/polybar/config.init 2>&1
+# polybar -c ~/.config/polybar/config.init 2>&1
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.init 2>&1 &
+done
